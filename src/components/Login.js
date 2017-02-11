732a85bbb23ffc80acd6b9fbc6c751418d9087bc
Joel Raju <joelraju@ymail.com>
2017-02-11 17:36:06 +0530

    * fixed login button size * fixed CI build error

--- src/components/Login.js
+++ src/components/Login.js
@@ -28,21 +28,29 @@ const LoginForm = Form.create()(React.createClass({
     	<Form onSubmit={this.handleSubmit} className="login-form">
 	        <FormItem>
 		    	{getFieldDecorator('userName', {
-		        	rules: [{ type:'email', required: true, message: 'Please input your username!' }],
+		        	rules: [{ type:'email', 
+		        			  required: true, 
+		        			  message: 'Please input your username!' }],
 		          })(
-		    		<Input addonBefore={<Icon type="user" />} placeholder="Username" />
+		    		<Input addonBefore={<Icon type="user" />} 
+		    			   placeholder="Username" />
 		    	)}
 	        </FormItem>
 	        <FormItem>
 	        	{getFieldDecorator('password', {
-	        		rules: [{ required: true, message: 'Please input your Password!' }],
+	        		rules: [{ required: true, 
+	        				  message: 'Please input your Password!' }],
 	        	})(
-	        		<Input addonBefore={<Icon type="lock" />} type="password" placeholder="Password" />
+	        		<Input addonBefore={<Icon type="lock" />} 
+	        			   type="password" 
+	        			   placeholder="Password" />
 	        	)}
 	        </FormItem>
 	        <FormItem>
 	          
-	          <Button type="primary" htmlType="submit" className="login-form-button">
+	          <Button type="primary" 
+	          		  htmlType="submit" 
+	          		  className="login-form-button">
 	            Log in
 	          </Button>
 	        </FormItem>
