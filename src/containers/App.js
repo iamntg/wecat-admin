307dae71f1eb29dae8222825fa2a0f4d61ab1894
Joel Raju <joelraju@ymail.com>
2017-02-12 20:38:06 +0530

    * added react-router * set up basic rotutes * added basic reducers and action types

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-
+import { Router, Route, Link, browserHistory } from 'react-router';
 
 import '../../styles/main.css';
 import Login from '../components/Login';
+import Dashboard from '../containers/Dashboard';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Login/>
-      </div>
+    	<Router history={browserHistory}>
+    		<Route path="/home/" component={Dashboard}>
+    			
+    		</Route>
+    		<Route path="*" component={Login}/>
+    	</Router>
     );
   }
 }
---
+++ src/containers/Dashboard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import '../../styles/main.css';
+
+
+class Dashboard extends Component {
+  render() {
+    return (
+    	<div>
+            <p>hello world</p>
+        </div>
+    );
+  }
+}
+
+export default Dashboard;
